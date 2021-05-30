a03a57e2da06a80d2d9f6596b016726c6545d8bc
Feta Deven <deshdeven@gmail.com>
2021-05-31 00:10:31 +0300

    removed unused link tag

--- src/layouts/documentLayouts/index.js
+++ src/layouts/documentLayouts/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { Button, message, Space } from 'antd';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { setDocuments,
          setDocumentsError,
          openAddDocumentsModal,
